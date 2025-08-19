5342d9e49a6c2bfa796af2d3b39388442a5793b1
andreasalbers <mail@andreasalbers.de>
2025-08-19 16:26:32 +0200

    2025-08-19 Version 3

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -10,7 +10,7 @@ new class extends Component
      */
     public function logout(Logout $logout): void
     {
-        
+
         $logout();
 
         $this->redirect('/', navigate: true);
@@ -93,6 +93,14 @@ new class extends Component
                 {{ __('Bestand') }}
             </x-responsive-nav-link>
         </div>
+
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('scanartikel')" :active="request()->routeIs('scanartikel')" wire:navigate>
+                {{ __('Scan Artikel') }}
+            </x-responsive-nav-link>
+        </div>
+
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -4,7 +4,7 @@
             href="{{ url('/bestand') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
-            Bestand
+            Bestandxx
         </a>
     @else
         <a
